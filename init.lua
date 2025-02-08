b6af138f08b3be43c984902475bf52d6c4986894
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-02-07 19:37:35 -0600

    add tailwindcss language server

--- init.lua
+++ init.lua
@@ -635,6 +635,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        tailwindcss = {
+          filetypes = { 'css', 'html', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
