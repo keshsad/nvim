eef4a22b3a28b363e84ce1d0ea91e37cc90df1c2
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-04-02 14:19:57 -0500

    add fugitive, hunks, and ai

--- init.lua
+++ init.lua
@@ -215,6 +215,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- j and k also use gj and gk
+vim.keymap.set('n', 'j', 'gj', { desc = 'Move down' })
+vim.keymap.set('n', 'k', 'gk', { desc = 'Move up' })
+vim.keymap.set('v', 'j', 'gj', { desc = 'Move down' })
+vim.keymap.set('v', 'k', 'gk', { desc = 'Move up' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -1031,7 +1037,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
