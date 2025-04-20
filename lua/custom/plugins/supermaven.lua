ed53589be6547e33f364a9fe5a7a61608a785e25
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-04-20 06:16:00 -0400

    ignore md for ai

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -2,7 +2,14 @@ return {
   {
     'supermaven-inc/supermaven-nvim',
     config = function()
-      require('supermaven-nvim').setup {}
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<Tab>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+        ignore_filetypes = { markdown = true }, -- or { "cpp", }
+      }
     end,
   },
 }
